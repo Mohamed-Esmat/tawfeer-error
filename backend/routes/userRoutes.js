073f6aa6a6d433efa366c2955cb62b21382635fa
Mohamed-Esmat-AbdElHafiz <msmt0452@gmail.com>
2022-06-30 18:18:25 +0200

    fix deleteUser

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -133,24 +133,24 @@ userRouter.put(
     }
   })
 );
-// userRouter.delete(
-//   '/:id',
-//   isAuth,
-//   isAdmin,
-//   expressAsyncHandler(async (req, res) => {
-//     const user = await User.findById(req.params.id);
-//     if (user) {
-//       if (user.email === 'admin@example.com') {
-//         res.status(400).send({ message: 'Can Not Delete Admin User' });
-//         return;
-//       }
-//       await user.remove();
-//       res.send({ message: 'User Deleted' });
-//     } else {
-//       res.status(404).send({ message: 'User Not Found' });
-//     }
-//   })
-// );
+userRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      if (user.email === 'admin@example.com') {
+        res.status(400).send({ message: 'Can Not Delete Admin User' });
+        return;
+      }
+      await user.remove();
+      res.send({ message: 'User Deleted' });
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
 
 
 export default userRouter;
